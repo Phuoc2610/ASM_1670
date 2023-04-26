0fbba798f251b12b9249f3795a8329b5f45ade4e
Phuoc2610 <phuocnvgcd191236@fpt.edu.vn>
2023-04-27 06:01:13 +0700

    OrderUpdate

--- ASM_1670/Controllers/OrdersController.cs
+++ ASM_1670/Controllers/OrdersController.cs
@@ -34,10 +34,93 @@ namespace ASM_APP_DEV.Controllers
             return View(oderInDb);
 
         }
-       
 
-		
+		[HttpGet]
+		[Authorize(Roles = "user")]
 
-  
-    }
+		public async Task<IActionResult> Create(int id)
+		{
+
+			var currentUser = await userManager.GetUserAsync(User);
+			var orderUnconfirmInDb = context.Orders.Include(o => o.OrderDetails)
+				.SingleOrDefault(o => o.OrderStatus == OrderStatus.Unconfirmed && o.UserId == currentUser.Id);
+
+			Book bookInDB = context.Books.SingleOrDefault(t => t.Id == id);
+			if (bookInDB.QuantityBook - 1 < 0)
+			{
+				return BadRequest();
+			}
+			if (orderUnconfirmInDb == null)
+			{
+
+				var order = new Order();
+				order.UserId = currentUser.Id;
+				order.OrderStatus = Enums.OrderStatus.Unconfirmed;
+				order.PriceOrder = bookInDB.PriceBook;
+				order.UserId = currentUser.Id;
+				context.Add(order);
+				context.SaveChanges();
+
+				OrderDetail orderDetail = new OrderDetail();
+				orderDetail.IdBook = bookInDB.Id;
+				orderDetail.IdOrder = order.Id;
+				orderDetail.Quantity = 1;
+				orderDetail.Price = bookInDB.PriceBook;
+				orderDetail.Order = order;
+				orderDetail.Book = bookInDB;
+
+				context.Add(orderDetail);
+				context.SaveChanges();
+				order.OrderDetails.Add(orderDetail);
+				context.SaveChanges();
+
+			}
+			else
+			{
+
+				var orderDetailInDb = orderUnconfirmInDb.OrderDetails.SingleOrDefault(o => o.IdBook == id);
+				if (orderDetailInDb == null)
+				{
+
+					OrderDetail orderDetail = new OrderDetail();
+					orderDetail.IdBook = bookInDB.Id;
+					orderDetail.IdOrder = orderUnconfirmInDb.Id;
+					orderDetail.Quantity = 1;
+
+					orderDetail.Price = bookInDB.PriceBook;
+					orderUnconfirmInDb.OrderDetails.Add(orderDetail);
+					context.Add(orderDetail);
+					context.SaveChanges();
+					orderUnconfirmInDb.PriceOrder = 0;
+					foreach (var item in orderUnconfirmInDb.OrderDetails)
+					{
+
+						orderUnconfirmInDb.PriceOrder += item.Price;
+					}
+				}
+				else
+				{
+					orderDetailInDb.Quantity += 1;
+
+					orderDetailInDb.Price = bookInDB.PriceBook * orderDetailInDb.Quantity;
+					orderUnconfirmInDb.PriceOrder = 0;
+					foreach (var orderDetail in orderUnconfirmInDb.OrderDetails)
+					{
+
+						orderUnconfirmInDb.PriceOrder += orderDetail.Price;
+					}
+				}
+				context.SaveChanges();
+
+			}
+			context.SaveChanges();
+
+			return RedirectToAction("Index", "OrderDetails");
+
+
+		}
+
+
+
+	}
 }
