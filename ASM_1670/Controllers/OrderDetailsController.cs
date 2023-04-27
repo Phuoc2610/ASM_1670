cb68f1265ad08d21a1ef5bbde544d890aa04dafc
Thai <thaidqgcd19963@fpt.edu.vn>
2023-04-28 03:15:03 +0700

    DeleteOrder

--- ASM_1670/Controllers/OrderDetailsController.cs
+++ ASM_1670/Controllers/OrderDetailsController.cs
@@ -29,11 +29,49 @@ namespace ASM_1670.Controllers
             viewModelCart.Address = currentUser.Address;
             viewModelCart.FullName = currentUser.FullName;
             viewModelCart.Phone = currentUser.PhoneNumber;
-            
+            if (id != 0)
+            {
+                var Order = context.Orders.SingleOrDefault(o => o.Id == id && o.UserId == currentUser.Id);
+
+                var orderDetails = context.OrderDetails
+                             .Where(t => t.Order.UserId == currentUser.Id && t.Order.Id == id).ToList();
+                foreach (var item in orderDetails)
+                {
+                    item.Book = context.Books.SingleOrDefault(b => b.Id == item.IdBook);
+                }
+                viewModelCart.OrderDetails = orderDetails;
+                viewModelCart.Order = Order;
+
+                return View(viewModelCart);
+            }
+            else
+            {
+                var orderDetails = context.OrderDetails.Include(t => t.Order)
+                    .Where(t => t.Order.OrderStatus == Enums.OrderStatus.Unconfirmed).ToList();
+
+                var Order = context.Orders.SingleOrDefault(o => o.OrderStatus == OrderStatus.Unconfirmed && o.UserId == currentUser.Id);
+                foreach (var item in orderDetails)
+                {
+                    item.Book = context.Books.SingleOrDefault(b => b.Id == item.IdBook);
+                }
+
+
+                viewModelCart.OrderDetails = orderDetails;
+                viewModelCart.Order = Order;
+            }
+            return View(viewModelCart);
 
         }
         [HttpGet]
 
+        public IActionResult Delete(int id)
+        {
+            var OrderDetail = context.OrderDetails.Include(t => t.Order)
+                .SingleOrDefault(t => t.Id == id && t.Order.OrderStatus == OrderStatus.Unconfirmed);
 
+            context.Remove(OrderDetail);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
-}
+} 
